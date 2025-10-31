d290a1cc7d0f02139b097ddea18e1cfd75f6671c
Sagetrash <sagetrash1435@proton.me>
2025-10-31 16:19:52 +0530

    reorganisation

---
+++ Java/old java/abstraction/abstraction.java
@@ -0,0 +1,13 @@
+class student{
+    public final String collegeName;
+    public student(){
+        collegeName = "YIT";
+    }
+}
+
+public class abstraction {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- Java/old java/reverse.java
+++ Java/old java/reverse.java
@@ -14,4 +14,4 @@ public class reverse {
         System.out.println(sum);
     }
     
-}
+} 
